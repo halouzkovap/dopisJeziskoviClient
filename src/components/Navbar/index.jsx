77881551b2e6d50edd96f987b99e23c094a8a778
petra <halouzkovap@seznam.cz>
2021-11-27 16:03:50 +0100

    add style

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import {
     Link
   } from 'react-router-dom';
+import './style.css';
 
 const Navbar =() =>{
     return(
         <nav className="navbar navbar-expand-lg navbar-light">
-        <Link className="navbar-brand" to="/">Přání ježíškovi</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/"><span className="nav_font_color">Přání ježíškovi</span></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Link className="nav-item nav-link active" to="/">Home</Link>
-            <Link className="nav-item nav-link" to="/Wishlist">Mé přání</Link>
-            <Link className="nav-item nav-link" to="/otherWishs">Přání ostatních</Link>
+            <Link className="nav-item nav-link active" to="/"><span className="nav_font_color">Home</span></Link>
+            <Link className="nav-item nav-link" to="/Wishlist"><span className="nav_font_color">Mé přání</span></Link>
+            <Link className="nav-item nav-link" to="/otherWishs"><span className="nav_font_color">Přání ostatních</span></Link>
           </div>
         </div>
       </nav>
--- src/components/NewWish/index.jsx
+++ src/components/NewWish/index.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import './style.css'
 
 class NewWish extends Component{
+    emptyItem = {
+        nazevPrani: ''
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+   
+    }
+
     render(){
+        const {item} = this.state;
         return(
-            <>
-            jen názvek přání
-            </>
+            <div className="new_wish_container">
+            <Container>
+                <h2 className="new_with_font">Přidat přání</h2>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="nazevPrani" className="new_with_font">Název přání</Label>
+                        <Input type="text" name="nazevPrani" id="nazevPrani" value={item.nazevPrani || ''}
+                               onChange={this.handleChange} autoComplete="name"/>
+                    </FormGroup>
+            
+                    <FormGroup>
+                        <Button  className="btn btn-light" type="submit">Save</Button>{' '}
+                        <Button className="btn btn-light"><Link to="/">Cancel</Link></Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
         )
     }
 }
