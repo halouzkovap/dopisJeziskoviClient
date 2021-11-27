62361def0ee54c96d26d68fbdd4e1259cf102ddb
petra <halouzkovap@seznam.cz>
2021-11-27 18:01:34 +0100

    change naming

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ import './style.css';
 const Navbar =() =>{
     return(
         <nav className="navbar navbar-expand-lg navbar-light">
-        <Link className="navbar-brand" to="/"><span className="nav_font_color">Dppis Ježíškovi</span></Link>
+        <Link className="navbar-brand" to="/"><span className="nav_font_color">Dopis Ježíškovi</span></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
